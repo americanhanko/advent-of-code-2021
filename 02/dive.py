a5fda6ecec4ed1208185a27cc985e2665980b470
Eric Hanko <eric.hanko@oracle.com>
2021-12-02 10:58:58 -0800

    Passing

--- 02/dive.py
+++ 02/dive.py
@@ -1,2 +1,13 @@
 def position(data):
-    return None
+    horizontal = 0
+    vertical = 0
+
+    for d in data:
+        if d[0] == "forward":
+            horizontal += d[1]
+        if d[0] == "down":
+            vertical += d[1]
+        if d[0] == "up":
+            vertical -= d[1]
+
+    return horizontal * vertical
