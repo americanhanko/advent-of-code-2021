58d59241763b4316c0f181db689954d0fb6769e5
Eric Hanko <eric.hanko@oracle.com>
2021-12-02 11:25:16 -0800

    Part two passing

--- 02/dive.py
+++ 02/dive.py
@@ -1,13 +1,34 @@
-def position(data):
+def position(data, use_aim: False):
     horizontal = 0
-    vertical = 0
+    depth = 0
+    aim = 0
 
     for d in data:
-        if d[0] == "forward":
-            horizontal += d[1]
-        if d[0] == "down":
-            vertical += d[1]
-        if d[0] == "up":
-            vertical -= d[1]
+        command = d[0]
+        units = int(d[1])
 
-    return horizontal * vertical
+        if use_aim:
+            if command == "forward":
+                horizontal += units
+                depth += units * aim
+            if command == "down":
+                aim += units
+            if command == "up":
+                aim -= units
+        else:
+            if command == "forward":
+                horizontal += units
+            if command == "down":
+                depth += units
+            if command == "up":
+                depth -= units
+
+    return horizontal * depth
+
+
+if __name__ == "__main__":
+    with open("input") as fp:
+        lines = fp.readlines()
+        instructions = [tuple(line.split()) for line in lines]
+
+    print(position(instructions, use_aim=True))
--- 02/tests/test_dive.py
+++ 02/tests/test_dive.py
@@ -1,14 +1,18 @@
 import dive
 
 data = [
-    ("forward", 5),
-    ("down", 5),
-    ("forward", 8),
-    ("up", 3),
-    ("down", 8),
-    ("forward", 2),
+    ("forward", "5"),
+    ("down", "5"),
+    ("forward", "8"),
+    ("up", "3"),
+    ("down", "8"),
+    ("forward", "2"),
 ]
 
 
 def test_dive():
-    assert dive.position(data) == 150
+    assert dive.position(data, use_aim=False) == 150
+
+
+def test_dive_with_aim():
+    assert dive.position(data, use_aim=True) == 900
