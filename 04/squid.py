c9e1d5b101097d904192a06b509cc3ed50fc0277
Eric Hanko <eric.hanko@oracle.com>
2021-12-04 12:45:34 -0800

    Correct answer for part I

--- 04/squid.py
+++ 04/squid.py
@@ -1,17 +1,69 @@
-def bingo(selections, boards):
-    return None
+import typing
+from typing import List
+
+
+def bingo(selections, board_data):
+    boards = [BingoBoard(data) for data in board_data]
+    for selection in selections:
+        for board in boards:
+            board.play_round(selection)
+            if board.check_for_winner():
+                return board.winning_result(selection)
 
 
 class BingoBoard:
-    def __init__(self, numbers):
-        self.rows = numbers
-        self.columns = self._create_columns(numbers)
+    def __init__(self, data):
+        self.data = data
+        self.width = len(data)
+        self.height = len(data[0])
+        self.rows = data
+        self.columns = self._create_columns(data)
+
+    def play_round(self, selection):
+        self.rows = [self.rm_selection(row, selection) for row in self.rows]
+        self.columns = [self.rm_selection(column, selection) for column in self.columns]
+
+    def check_for_winner(self):
+        return not all(self.rows) or not all(self.columns)
 
     @staticmethod
-    def _create_columns(rows):
+    def rm_selection(line, selection):
+        try:
+            line.remove(selection)
+        except ValueError:
+            pass
+        return line
+
+    @staticmethod
+    def _create_columns(data):
         columns = []
-        for i in range(len(rows)):
+        for i in range(len(data)):
             columns.append([])
-            for j in range(len(rows[0])):
-                columns[i].append(rows[j][i])
+            for j in range(len(data[0])):
+                columns[i].append(data[j][i])
         return columns
+
+    def winning_result(self, selection):
+        return sum([sum([int(i) for i in row]) for row in self.rows]) * int(selection)
+
+
+if __name__ == "__main__":
+    with open("input") as fp:
+        content = fp.read().strip().split("\n\n")
+
+    def selections():
+        return [i for i in content[0].split(",")]
+
+    def all_boards():
+        board_data = content[1:]
+        boards = [board.split("\n") for board in board_data]
+        all_boards = []
+
+        for board in boards:
+            b = [row.split() for row in board]
+            all_boards.append(b)
+
+        return all_boards
+
+    result = bingo(selections(), all_boards())
+    print(result)
--- 04/tests/test_squid.py
+++ 04/tests/test_squid.py
@@ -5,7 +5,6 @@ import squid
 from squid import BingoBoard
 
 
-@pytest.mark.skip()
 def test_bingo_winner(selections, all_boards):
     assert squid.bingo(selections, all_boards) == 4512
 
@@ -19,4 +18,52 @@ def test_creating_bingo_board(all_boards):
         ["17", "23", "14", "3", "20"],
         ["11", "4", "16", "18", "15"],
         ["0", "24", "7", "5", "19"],
+    ]
+
+
+def test_checking_marking_selection(all_boards):
+    first = all_boards[0]
+    b = BingoBoard(first)
+    b.play_round("22")
+
+    assert b.rows == [
+        ["13", "17", "11", "0"],
+        ["8", "2", "23", "4", "24"],
+        ["21", "9", "14", "16", "7"],
+        ["6", "10", "3", "18", "5"],
+        ["1", "12", "20", "15", "19"],
+    ]
+
+    assert b.columns == [
+        ["8", "21", "6", "1"],
+        ["13", "2", "9", "10", "12"],
+        ["17", "23", "14", "3", "20"],
+        ["11", "4", "16", "18", "15"],
+        ["0", "24", "7", "5", "19"],
+    ]
+
+
+def test_checking_winning_board(selections, all_boards):
+    board = all_boards[2]
+    b = BingoBoard(board)
+
+    for selection in selections:
+        b.play_round(selection)
+        if b.check_for_winner():
+            break
+
+    assert b.rows == [
+        [],
+        ["10", "16", "15", "19"],
+        ["18", "8", "26", "20"],
+        ["22", "13", "6"],
+        ["12", "3"],
+    ]
+
+    assert b.columns == [
+        ["10", "18", "22"],
+        ["16", "8"],
+        ["15", "13", "12"],
+        ["26", "6", "3"],
+        ["19", "20"],
     ]
