71a85c20ed2999b48d87e8acbc0ebdd124e0080e
Eric Hanko <eric.hanko@oracle.com>
2021-12-05 12:17:22 -0800

    Passing Day 3, part 1

---
+++ 03/diagnostic.py
@@ -0,0 +1,14 @@
+class BinaryDiagnostic:
+    def __init__(self, data):
+        self._data = data.strip().split("\n")
+        self.gamma = self._common_bit_as_int(max)
+        self.epsilon = self._common_bit_as_int(min)
+        self.power_consumption = self.gamma * self.epsilon
+
+    def _transpose(self):
+        nums = [list(d) for d in self._data]
+        return [list(col) for col in zip(*nums)]
+
+    def _common_bit_as_int(self, func):
+        as_bin = "".join([func(set(col), key=col.count) for col in self._transpose()])
+        return int(as_bin, base=2)
