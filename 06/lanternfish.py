301b03977b1a13c7ea55f5e42948729d496d99db
Eric Hanko <eric.hanko@oracle.com>
2021-12-06 19:49:31 -0800

    Passing interim test

--- 06/lanternfish.py
+++ 06/lanternfish.py
@@ -1,5 +1,13 @@
-def growth_rate(data) -> int:
-    return 0
+def growth_rate(data, days=0) -> int:
+    init_fish = data.split(",")
+
+    days = days
+    fish = init_fish
+
+    while days:
+        days -= 1
+
+    return len(fish)
 
 
 if __name__ == "__main__":
--- 06/tests/test_lanternfish.py
+++ 06/tests/test_lanternfish.py
@@ -3,5 +3,9 @@ import lanternfish
 fish = "3,4,3,1,2"
 
 
+def test_lanternfish_interim():
+    assert lanternfish.growth_rate(fish, days=1)
+
+
 def test_lanternfish():
-    assert lanternfish.growth_rate(fish) == 5934
+    assert lanternfish.growth_rate(fish, days=80) == 5934
