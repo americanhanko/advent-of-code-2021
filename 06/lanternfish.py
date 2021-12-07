3001c1a4c1c4c438c169f52221b7a1270e4825c2
Eric Hanko <eric.hanko@oracle.com>
2021-12-07 11:50:59 -0800

    Passing tests for Day 6

--- 06/lanternfish.py
+++ 06/lanternfish.py
@@ -1,22 +1,26 @@
 from typing import List
 
 
-def growth_rate(data: List[int], days=0) -> int:
-    days = days
-    fish = data
-
-    print(days)
-    while days > 0:
-        fish = [f - 1 for f in fish]
+def growth_rate(fish: List[int], days) -> int:
+    if days > 0:
+        next_gen = []
+        for i in range(len(fish)):
+            if fish[i] != 0:
+                aged = fish[i] - 1
+                next_gen.insert(i, aged)
+            else:
+                next_gen.append(8)
+                next_gen.insert(i, 6)
         days -= 1
-
-    return growth_rate(fish, days)
+        return growth_rate(next_gen, days)
+    else:
+        return len(fish)
 
 
 if __name__ == "__main__":
     with open("input") as fp:
         content = fp.read().strip()
 
-    init_fish = [int(i) for i in content.split(",")]
-    result = growth_rate(init_fish)
+    fish_data = [int(i) for i in content.split(",")]
+    result = growth_rate(fish_data, days=80)
     print(result)
--- 06/tests/test_lanternfish.py
+++ 06/tests/test_lanternfish.py
@@ -1,14 +1,15 @@
-import pytest
-
 import lanternfish
 
 fish = [int(d) for d in "3,4,3,1,2".split(",")]
 
 
-def test_lanternfish_interim():
-    assert lanternfish.growth_rate(fish, days=2)
+def test_lanternfish_interim_short():
+    assert lanternfish.growth_rate(fish, days=2) == 6
 
 
-@pytest.mark.skip
+def test_lanternfish_interim_medium():
+    assert lanternfish.growth_rate(fish, days=18) == 26
+
+
 def test_lanternfish():
     assert lanternfish.growth_rate(fish, days=80) == 5934
