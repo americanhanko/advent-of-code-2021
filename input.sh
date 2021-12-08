1a7f11594bfa8ba4e257b5cbe1f9a6c8f1d1c18e
Eric Hanko <eric.hanko@oracle.com>
2021-12-07 16:13:18 -0800

    Fix input.sh script

--- input.sh
+++ input.sh
@@ -7,7 +7,5 @@ if [[ -z "${day}" ]]; then
     exit 0
 fi
 
-puzzle_input=$(curl -fsSL --cookie cookies.txt https://adventofcode.com/2021/day/"${day}"/input)
-
-printf -v day_dir "%02d\n" "${day}"
-echo "${puzzle_input}" > "${day_dir}"/input
+printf -v day_dir "%02d" "${day}"
+curl -fsSL --cookie cookies.txt https://adventofcode.com/2021/day/"${day}"/input --output "${day_dir}"/input
